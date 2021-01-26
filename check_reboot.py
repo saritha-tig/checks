15ce4a6783a731a0b5226ddaedce8705ba9dd6f8
Saru Gupta <sarithagupta125@gmail.com>
2021-01-25 22:50:20 -0800

    Added 4 files

---
+++ check_reboot.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+def check_reboot():
+    """Returns true if the computer has a pending reboot"""
+    return os.path.exist("/run/reboot-required")
+
+
+def main()
+    if check_reboot():
+        print("Pending Reboot:")
+        sys.exit(1)
+    print("Everything ok.")
+    sys.exit(0)
+
+
+main()
---
+++ free_memory.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+<<<<<<< HEAD
+    '''Caliculating free memory'''
+    print("I am fine")
+
+main()
